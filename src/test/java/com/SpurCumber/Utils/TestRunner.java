941c5b5308ee09d883c633a80136df33e2a11cd9
Sandesh <sandesh.jain@spurqlabs.com>
2023-11-16 13:32:29 +0530

    Updated Test Runner

--- src/test/java/com/SpurCumber/Utils/TestRunner.java
+++ src/test/java/com/SpurCumber/Utils/TestRunner.java
@@ -13,7 +13,7 @@ import static java.lang.Thread.sleep;
 
 @CucumberOptions(features = {"src/test/java/com/SpurCumber/Features"},
         glue = "com.SpurCumber.Steps",
-        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","pretty", "json:test-output/Cucumber.json", "pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm" })
+        plugin = {"json:test-output/Cucumber.json", "pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm" })
 public class TestRunner extends AbstractTestNGCucumberTests {
     private final String REPORT_PATH = "test-output/ZAP_Report.html";
     private String SecurityScanMode = null;
@@ -41,22 +41,22 @@ public class TestRunner extends AbstractTestNGCucumberTests {
 
     }
 
-//    @AfterSuite
-//    public void AfterSuite() {
-//        try {
-//            SecurityScanMode = ConfigUtil.getPropValues("SecurityScan");
-//            if (SecurityScanMode.equals("ON")) {
-//                if (ConfigUtil.getPropValues("FrameworkMode").equals("Web"))
-//                    ZapUtil.ActiveScan(ConfigUtil.getPropValues("AppURL"));
-//                else if (ConfigUtil.getPropValues("FrameworkMode").equals("API"))
-//                    ZapUtil.ActiveScan(ConfigUtil.getPropValues("baseURL"));
-//                ZapUtil.ShutdownZAP();
-//                ZapUtil.WriteHtmlReport(REPORT_PATH);
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
+    @AfterSuite
+    public void AfterSuite() {
+        try {
+            SecurityScanMode = ConfigUtil.getPropValues("SecurityScan");
+            if (SecurityScanMode.equals("ON")) {
+                if (ConfigUtil.getPropValues("FrameworkMode").equals("Web"))
+                    ZapUtil.ActiveScan(ConfigUtil.getPropValues("AppURL"));
+                else if (ConfigUtil.getPropValues("FrameworkMode").equals("API"))
+                    ZapUtil.ActiveScan(ConfigUtil.getPropValues("baseURL"));
+                ZapUtil.ShutdownZAP();
+                ZapUtil.WriteHtmlReport(REPORT_PATH);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
 
 
